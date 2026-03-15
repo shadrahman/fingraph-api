c8cb45f6af3ee38cffe99ad5411da6c328eeeb2c
shadman rahman <shadmanr.1992@gmail.com>
2026-03-15 20:15:04 +1100

    chore: initial project setup with FinGraph naming and metadata

---
+++ src/main/java/io/github/shadrahman/fingraph/FingraphApplication.java
@@ -0,0 +1,13 @@
+package io.github.shadrahman.fingraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/shadrahman/fingraph/FingraphApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shadrahman.fingraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
